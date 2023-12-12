81f22b9522ed9fc4826bf453b5c7fff17b04cb94
Drivahah <s212975@student.dtu.com>
2023-12-12 13:04:31 +0100

    New nestedCV, train/predict options

--- notebooks/pipe.py
+++ notebooks/pipe.py
@@ -13,7 +13,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV, cross_val_score
+from sklearn.model_selection import GridSearchCV, cross_val_score, StratifiedKFold
 import joblib
 from imblearn.pipeline import Pipeline as ImbPipeline
 from imblearn.over_sampling import SMOTE, ADASYN
@@ -37,12 +37,14 @@ with open('A.txt', 'a') as f:
 parser = argparse.ArgumentParser()
 parser.add_argument('--embedder', type=str, choices=['prott5', 'protxlnet'], default='prott5', help='Embedder to use')
 parser.add_argument('--fine_tune', action='store_true', help='Whether to fine-tune the embedder')
-parser.add_argument('--estimator', type=str, choices=['logreg', 'rf', 'transformer'], default='logreg', help='Final estimator to use')
-parser.add_argument('--load_embedder', action='store_true', help='Whether to load pre-trained parameters for the embedder')
-parser.add_argument('--load_estimator', action='store_true', help='Whether to load pre-trained parameters for the final estimator')
 parser.add_argument('--oversampling', type=str, choices=['smote', 'adasyn', 'none'], default='none', help='Oversampling technique to use for imbalanced data')
+parser.add_argument('--estimator', type=str, choices=['logreg', 'rf', 'transformer'], default='logreg', help='Final estimator to use')
+# parser.add_argument('--load_embedder', action='store_true', help='Whether to load pre-trained parameters for the embedder')
+# parser.add_argument('--load_estimator', action='store_true', help='Whether to load pre-trained parameters for the final estimator')
+parser.add_argument('--train', action='store_true', help='Whether to train the whole model')
 parser.add_argument('--grid_search', action='store_true', help='Whether to perform grid search for the pipeline')
 parser.add_argument('--param_grid', type=str, default='{}', help='Parameter grid for the grid search as a string')
+parser.add_argument('--predict', type=str, help='Specify the pre-trained model to use to predict on the given data')
 parser.add_argument('--epochs', type=int, default=10, help='Number of epochs for fine-tuning the embedder')
 parser.add_argument('--steps', type=int, default=10, help='Number of steps to train the embedder before freezing the parameters')
 parser.add_argument('--lr_embedder', type=float, default=0.01, help='Learning rate for the embedder')
@@ -157,10 +159,10 @@ if args.embedder == 'prott5':
 elif args.embedder == 'protxlnet':
     embedder = ProtXLNetEmbedder('Rostlab/prot_xlnet', fine_tune=args.fine_tune, device=args.device)
 
-if args.load_embedder:
-    # load pre-trained parameters for the embedder
-    embedder.load_state_dict(torch.load('embedder.pth'))
-    embedder.eval()
+# if args.load_embedder:
+#     # load pre-trained parameters for the embedder
+#     embedder.load_state_dict(torch.load('embedder.pth'))
+#     embedder.eval()
 
 if args.estimator == 'logreg':
     estimator = LogisticRegression()
@@ -170,9 +172,9 @@ elif args.estimator == 'rf':
 #     # use a transformer model as the final estimator
 #     estimator = TransformerClassifier(device=args.device)
 
-if args.load_estimator:
-    # load pre-trained parameters for the final estimator
-    estimator = joblib.load(args.estimator + '.pkl')
+# if args.load_estimator:
+#     # load pre-trained parameters for the final estimator
+#     estimator = joblib.load(args.estimator + '.pkl')
 
 if args.oversampling == 'smote':
     # use SMOTE for imbalanced data
@@ -217,7 +219,7 @@ file_handler.setFormatter(formatter)
 # add the file handler to the logger
 logger.addHandler(file_handler)
 # log the pipeline options
-logger.info(f'Pipeline options: embedder={args.embedder}, fine_tune={args.fine_tune}, estimator={args.estimator}, load_embedder={args.load_embedder}, load_estimator={args.load_estimator}, oversampling={args.oversampling}, grid_search={args.grid_search}, param_grid={args.param_grid}, epochs={args.epochs}, steps={args.steps}, lr_embedder={args.lr_embedder}, lr_fine_tuned={args.lr_fine_tuned}, batch_size={args.batch_size}')
+logger.info(f'Pipeline options: embedder={args.embedder}, fine_tune={args.fine_tune}, estimator={args.estimator}, train={args.train}, grid_search={args.grid_search}, param_grid={args.param_grid}, epochs={args.epochs}, steps={args.steps}, lr_embedder={args.lr_embedder}, lr_fine_tuned={args.lr_fine_tuned}, batch_size={args.batch_size}, device={args.device}, debug={args.debug}, quick={args.quick}')
 
 # load the data
 INPUT_FOLDER = os.path.join('..', 'data', 'interim')
@@ -228,26 +230,100 @@ logger.info(f'Data shape: X={X.shape}, y={y.shape}')
 
 # perform nested cross-validation
 param_grid = eval(args.param_grid) # convert the string to a dictionary
-if args.grid_search:
-    # perform grid search for the pipeline
-    logger.debug('Performing grid search for the pipeline')
-    grid = GridSearchCV(pipe, param_grid, cv=5)
-    scores = cross_val_score(grid, X, y, cv=5)
-    # log the best parameters and score from the grid search
-    if hasattr(grid, 'best_params_'):
-        best_params = grid.best_params_
-    else:
-        best_params = grid.best_estimator_.get_params()
-    logger.debug(f'Best parameters: {best_params}')
-    logger.debug(f'Best score: {grid.best_score_}')
-else:
-    # use the default parameters for the pipeline
-    logger.debug('Using the default parameters for the pipeline')
-    scores = cross_val_score(pipe, X, y, cv=5)
+# if args.grid_search:
+#     # perform grid search for the pipeline
+#     logger.debug('Performing grid search for the pipeline')
+#     grid = GridSearchCV(pipe, param_grid, cv=5)
+#     scores = cross_val_score(grid, X, y, cv=5)
+#     # log the best parameters and score from the grid search
+#     if hasattr(grid, 'best_params_'):
+#         best_params = grid.best_params_
+#     else:
+#         best_params = grid.best_estimator_.get_params()
+#     logger.debug(f'Best parameters: {best_params}')
+#     logger.debug(f'Best score: {grid.best_score_}')
+# else:
+#     # use the default parameters for the pipeline
+#     logger.debug('Using the default parameters for the pipeline')
+#     scores = cross_val_score(pipe, X, y, cv=5)
 
-# log the nested cross-validation scores and the mean score
-logger.info(f'Nested cross-validation scores: {scores}')
-logger.info(f'Mean score: {scores.mean()}')
+# # log the nested cross-validation scores and the mean score
+# logger.info(f'Nested cross-validation scores: {scores}')
+# logger.info(f'Mean score: {scores.mean()}')
+
+
+if args.train:
+    best_outer_score = float('-inf')  # Initialize with a very small value for maximization
+    best_model = None
+    best_params = None
+
+    # Outer cross-validation
+    outer_cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+    outer_scores = []
+
+    for fold, (train_index, test_index) in enumerate(outer_cv.split(X, y)):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+
+        if args.grid_search:
+            # Inner cross-validation for grid search
+            inner_cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+            grid = GridSearchCV(pipe, param_grid, cv=inner_cv)
+            grid.fit(X_train, y_train)
+
+            # Evaluate the best model on the test set
+            score = grid.score(X_test, y_test)
+            outer_scores.append(score)
+
+            # Log the best parameters and score from the grid search for this fold
+            logger.debug(f'Best parameters for fold {fold+1}: {grid.best_params_}')
+            logger.debug(f'Score for fold {fold+1}: {score}')
+
+            # Check if this model has a better score than the current best
+            if score > best_outer_score:
+                best_outer_score = score
+                best_model = grid.best_estimator_
+                best_params = grid.best_params_
+
+        else:
+            # Fit the pipeline on the training data without grid search
+            pipe.fit(X_train, y_train)
+
+            # Evaluate the pipeline on the test set
+            score = pipe.score(X_test, y_test)
+            outer_scores.append(score)
+
+            # Log the score for this fold
+            logger.debug(f'Score for fold {fold+1}: {score}')
+
+    # Save the best model
+    if best_model is not None:
+        # Create a name for the best model based on configuration and timestamp
+        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+        model_name = f'best_model_{args.embedder}_{"fine_tune" if args.fine_tune else "no_fine_tune"}_{args.oversampling}_{args.estimator}_{timestamp}.pkl'
+        joblib.dump(best_model, model_name)
+        logger.debug(f'Best parameters across all folds: {best_params}')
+        logger.debug(f'Best score across all folds: {best_outer_score}')
+        logger.debug(f'Best model saved as: {model_name}')
+    else:
+        logger.warning('No best model found.')
+
+    # Log the results of nested cross-validation
+    logger.debug(f'Nested cross-validation scores: {outer_scores}')
+    logger.debug(f'Mean score: {np.mean(outer_scores)}')
+
+# Load model
+# loaded_model = joblib.load('best_model.joblib')
+
+# Make predictions with the loaded model
+# predictions = loaded_model.predict(new_data)
+
+if args.predict:
+    loaded_model = joblib.load(args.predict)
+    predictions = loaded_model.predict(X)
+    # Save predictions in a file
+    np.savetxt('predictions.txt', predictions, fmt='%s')
 
 # Print end in a file
 with open('A.txt', 'a') as f:
