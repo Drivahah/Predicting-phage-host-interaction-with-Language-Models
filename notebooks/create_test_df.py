cab68fe55e00106c5e67bf4714e08843a9fc2849
Drivahah <s212975@student.dtu.com>
2023-12-23 17:06:05 +0100

    add pair

--- notebooks/create_test_df.py
+++ notebooks/create_test_df.py
@@ -10,13 +10,17 @@ import os
 def random_seq(length):
     return ''.join(random.choice(string.ascii_uppercase) for i in range(length))
 
-# Make a df with two columns, phage and k12
+# Make a df with three columns, sequence_phage, sequence_k12, and pair
 # It has 100 rows
-# The phage column has random sequences of length between 5 and 25
-# The k12 column has random sequences of length between 5 and 25
+# The sequence_phage column has random sequences of length between 5 and 25
+# The sequence_k12 column has random sequences of length between 5 and 25
+# The pair column has random 1 and 0
 
-df = pd.DataFrame({'sequence_phage': [random_seq(random.randint(5,25)) for i in range(100)],
-                     'sequence_k12': [random_seq(random.randint(5,25)) for i in range(100)]})
+df = pd.DataFrame({
+    'sequence_phage': [random_seq(random.randint(5,25)) for i in range(100)],
+    'sequence_k12': [random_seq(random.randint(5,25)) for i in range(100)],
+    'pair': np.random.randint(2, size=100)
+})
 
 # Define savedir data/interim using os
 savedir = os.path.join('data', 'interim')
