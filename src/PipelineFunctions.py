8031b54e5c4c44334a98479a2a1b3264c9927009
Drivahah <s212975@student.dtu.com>
2024-01-04 16:28:58 +0100

    Define CustomRandomForest arguments explicitely

--- src/PipelineFunctions.py
+++ src/PipelineFunctions.py
@@ -248,8 +248,8 @@ def calculate_metrics(estimator, X_train, y_train, X_test, y_test):
     return metrics
 
 class CustomRandomForestClassifier(RandomForestClassifier):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, n_estimators=100, criterion='gini', max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features='auto', max_leaf_nodes=None, min_impurity_decrease=0.0, min_impurity_split=None, bootstrap=True, oob_score=False, n_jobs=None, random_state=None, verbose=0, warm_start=False, class_weight=None, ccp_alpha=0.0, max_samples=None):
+        super().__init__(n_estimators=n_estimators, criterion=criterion, max_depth=max_depth, min_samples_split=min_samples_split, min_samples_leaf=min_samples_leaf, min_weight_fraction_leaf=min_weight_fraction_leaf, max_features=max_features, max_leaf_nodes=max_leaf_nodes, min_impurity_decrease=min_impurity_decrease, min_impurity_split=min_impurity_split, bootstrap=bootstrap, oob_score=oob_score, n_jobs=n_jobs, random_state=random_state, verbose=verbose, warm_start=warm_start, class_weight=class_weight, ccp_alpha=ccp_alpha, max_samples=max_samples)
         self.metrics = []
 
     def fit(self, X, y, X_val, y_val):
@@ -267,4 +267,4 @@ def plot_metrics(metrics, metric_name, save_path):
     plt.title(f'Train vs Test {metric_name.capitalize()} Over Time')
     plt.legend()
     plt.savefig(save_path)
-    plt.close()  # Close the plot to avoid displaying it inline if not desired
+    plt.close()  # Close the plot to avoid displaying it inline if not desired  
