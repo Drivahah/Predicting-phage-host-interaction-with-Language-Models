53e0640d9faa06d6522f283933ce4cdaf9d1b054
Drivahah <s212975@student.dtu.com>
2024-01-06 17:18:16 +0100

    Remove mean_test_score line

--- notebooks/pipe.py
+++ notebooks/pipe.py
@@ -191,11 +191,10 @@ if args.train:
             grid.fit(X_train, y_train)
             score = grid.score(X_test, y_test)
             logger.debug(f'Best parameters: {grid.best_params_}')
-            logger.debug(f'Scores {refit} for the best model: {grid.cv_results_["mean_test_score_{refit}"]}')
+            # logger.debug(f'Scores {refit} for the best model: {grid.cv_results_["mean_test_score_{refit}"]}')
             logger.debug(f'Score for {refit}: {score}')
             for metric in scoring:
-                if metric != refit:
-                    logger.debug(f'Score for {metric}: {grid.cv_results_[f"mean_test_{metric}"]}')
+                logger.debug(f'Score for {metric}: {grid.cv_results_[f"mean_test_{metric}"]}')
         # Fit the pipeline on the training data without grid search and inner CV
         else:
             pipe2.fit(X_train, y_train)
