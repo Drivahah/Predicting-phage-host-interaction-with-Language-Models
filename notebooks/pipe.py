2d49404e70c0b925cdcb5c4291b04ca1a6d79680
Drivahah <s212975@student.dtu.com>
2024-02-11 16:49:25 +0100

    Convert labels to index tensor and update one-hot encoding

--- notebooks/pipe.py
+++ notebooks/pipe.py
@@ -422,7 +422,8 @@ if args.train:
                     outputs = model(inputs)
                     labels = labels.to(outputs.device)  # Move labels tensor to the same device as outputs
                     labels = labels.view(-1, 1)  # Reshape labels tensor to match the shape of outputs tensor
-                    labels = F.one_hot(labels, num_classes=2)  # Convert labels to one-hot binary
+                    labels = labels.long()  # Convert labels to index tensor
+                    labels = F.one_hot(labels.squeeze(), num_classes=2)  # Convert labels to one-hot binary
                     logger.info(f"Outputs: {outputs}")
                     logger.info(f"Labels: {labels}")
                     loss = criterion(outputs, labels)
