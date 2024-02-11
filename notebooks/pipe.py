30fa2313d447ac30103cd28c6f7f1931636de8bb
Drivahah <s212975@student.dtu.com>
2024-02-12 00:01:34 +0100

    Fix loss calculation in training loop

--- notebooks/pipe.py
+++ notebooks/pipe.py
@@ -426,7 +426,7 @@ if args.train:
                     labels = F.one_hot(labels, num_classes=2)  # Convert labels to one-hot binary
                     logger.info(f"Outputs: {outputs}")
                     logger.info(f"Labels: {labels}")
-                    loss = criterion(outputs, labels.float().to(outputs.device) )
+                    loss = criterion(outputs[:,1], labels.float())
                     loss.backward()
                     optimizer.step()
                     running_loss += loss.item() * inputs.size(0)
