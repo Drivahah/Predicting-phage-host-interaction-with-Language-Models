5d4ef078aaf1d5c4faeeb2db7fd5f903112658f1
Drivahah <s212975@student.dtu.com>
2024-02-11 18:10:21 +0100

    Remove label reshaping code

--- notebooks/pipe.py
+++ notebooks/pipe.py
@@ -421,7 +421,7 @@ if args.train:
                     optimizer.zero_grad()
                     outputs = model(inputs)
                     labels = labels.to(outputs.device)  # Move labels tensor to the same device as outputs
-                    labels = labels.view(-1, 1)  # Reshape labels tensor to match the shape of outputs tensor
+                    # labels = labels.view(-1, 1)  # Reshape labels tensor to match the shape of outputs tensor
                     labels = labels.long()  # Convert labels to index tensor
                     labels = F.one_hot(labels, num_classes=2)  # Convert labels to one-hot binary
                     logger.info(f"Outputs: {outputs}")
